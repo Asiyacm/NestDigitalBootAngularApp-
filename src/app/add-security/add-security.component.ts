4d1a7d9c7090151c9782c7788fb8f3cc90259627
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-15 12:28:30 +0530

    addSecurity commit

---
+++ src/app/add-security/add-security.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-security',
+  templateUrl: './add-security.component.html',
+  styleUrls: ['./add-security.component.css']
+})
+export class AddSecurityComponent {
+
+  empCode=""
+  name=""
+  username=""
+  password=""
+
+
+  readValues=()=>
+  {
+    let data:any={
+      "empCode":this.empCode,"name":this.name,"username":this.username,"password":this.password
+    }
+    console.log(data)
+  
+  }
+
+}
