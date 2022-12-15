c846027f038556cb710e720494cd4a4fd24e374c
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-15 19:47:24 +0530

    api commit

--- src/app/add-security/add-security.component.ts
+++ src/app/add-security/add-security.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-security',
@@ -12,6 +13,9 @@ export class AddSecurityComponent {
   username=""
   password=""
 
+  
+  constructor(private api:ApiService){}
+
 
   readValues=()=>
   {
@@ -19,7 +23,25 @@ export class AddSecurityComponent {
       "empCode":this.empCode,"name":this.name,"username":this.username,"password":this.password
     }
     console.log(data)
-  
+
+    this.api.SecurityAdd(data).subscribe(
+      (response:any)=>
+      {
+        console.log(response)
+        if (response.status=="success")
+        {
+          alert("Security added successfully")
+          this.empCode=""
+          this.name=""
+          this.username=""
+          this.password=""
+        }
+        else{
+          alert("Something went wrong")
+        }
+      }
+    )
   }
+    
 
 }
