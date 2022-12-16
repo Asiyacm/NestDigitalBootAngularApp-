9ce5f93dc57b842a4e078143ba11603be3c954f2
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-16 18:19:22 +0530

    security nav commit

---
+++ src/app/security-navigation/security-navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-navigation',
+  templateUrl: './security-navigation.component.html',
+  styleUrls: ['./security-navigation.component.css']
+})
+export class SecurityNavigationComponent {
+
+}
